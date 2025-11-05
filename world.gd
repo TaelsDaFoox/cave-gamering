cf18c2e6f2dba4424fc3698243ae2f65a8e5d6d1
TaelsDaFoox <bazmulligan@gmail.com>
2025-11-04 19:36:03 -0800

    rework, its animal crossing now
    wasn't vibing with trying to make a minecraft clone so instead you get gamecube animal crossing

--- world.gd
+++ world.gd
@@ -1,11 +1,12 @@
 extends Node3D
 var block = load("res://objects/block.tscn")
 @onready var csgCombiner = $CSGCombiner3D
+var worldsize := 16
 
 func _ready() -> void:
-	for i in 10:
-		for j in 10:
+	for i in 16:
+		for j in 16:
 			var blockspawn = block.instantiate()
-			blockspawn.position.x=i
-			blockspawn.position.z=j
+			blockspawn.position.x=i-16/2
+			blockspawn.position.z=j-16/2
 			csgCombiner.add_child(blockspawn)
