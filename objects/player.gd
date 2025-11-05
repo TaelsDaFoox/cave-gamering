635cab77c05434dd2cc352707aa974a76f5efc2d
TaelsDaFoox <bazmulligan@gmail.com>
2025-11-04 20:25:00 -0800

    walls, player rotation, cube
    wall collision, player matches movement direction, cube in room as a placeholder for movable furnature

--- objects/player.gd
+++ objects/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 #@onready var camera = $Camera3D
-@export var move_speed :=10
+@export var move_speed :=8
+@onready var model = $model
 func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("left","right","forward","backward")
+	if input_dir:
+		model.rotation.y=-input_dir.angle()-PI/2
 	#input_dir = input_dir.rotated(-camera.rotation.y)
 	velocity.x=input_dir.x*move_speed
 	velocity.z=input_dir.y*move_speed
