8bab33801e058f1d106cc8f5bdc044ff7be90197
TaelsDaFoox <bazmulligan@gmail.com>
2025-11-09 00:32:01 -0800

    randomizable carpet (press 2)
    random shirt button move to 1 key. randomizing the walls will probably be 3 and so on until I make these kinds of things actual inventory items (gonna add an inventory next)

--- models/carpet.gd
+++ models/carpet.gd
@@ -1,5 +1,11 @@
 extends Node3D
 @onready var mesh = $Plane
-var carpettex = load("res://materials/shirt.tres")
+var carpettex = load("res://materials/carpet.tres")
 func _ready() -> void:
 	mesh.set_surface_override_material(0,carpettex)
+	setCarpet(randi_range(0,67))
+func setCarpet(carpetnum:int):
+	carpettex.uv1_offset=Vector3(0.125*carpetnum,0.111*floor(carpetnum/9.0),0.0)
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("NewCarpet"):
+		setCarpet(randi_range(0,67))
